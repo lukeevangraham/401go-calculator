6353c6981d1794a9e93b739f077c1e254f4558ef
Luke Graham <luke.evan.graham@gmail.com>
2023-03-17 22:52:45 -0700

    calculating employees

--- calculator.js
+++ calculator.js
@@ -1,22 +1,24 @@
+const form = document.querySelector("#calcForm");
+const totalAnnualCost = document.querySelector("#totalAnnualCost");
 const amountOfW2Employees_input = document.querySelector(
   "#amountOfW2Employees"
 );
 const amountOfW2Employees_value = document.querySelector(
   "#amountOfW2EmployeesValue"
 );
-const amountOfOwners_input = document.querySelector(
-  "#amountOfOwners"
-);
-const amountOfOwners_value = document.querySelector(
-  "#amountOfOwnersValue"
-);
+const amountOfOwners_input = document.querySelector("#amountOfOwners");
+const amountOfOwners_value = document.querySelector("#amountOfOwnersValue");
 const amountOfHighEarners_input = document.querySelector(
   "#amountOfHighEarners"
 );
 const amountOfHighEarners_value = document.querySelector(
   "#amountOfHighEarnersValue"
 );
+const planSelected = document.querySelector("#planSelected");
 
+function numberWithCommas(x) {
+  return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
 
 // RESPOND TO SLIDER INPUT FOR NUMBER OF EMPLOYEES
 amountOfW2Employees_input.addEventListener("input", (e) => {
@@ -33,10 +35,56 @@ amountOfHighEarners_input.addEventListener("input", (e) => {
   amountOfHighEarners_value.textContent = e.target.value;
 });
 
+// RESPOND TO FORM CHANGES
 
+form.addEventListener("change", (e) => {
+  let participantMultiplier = 9;
 
+  const employeesEntered = Number(amountOfW2Employees_input.value);
 
+  if (employeesEntered > 50) {
+    if (employeesEntered <= 100) {
+      participantMultiplier = 8;
+    } else if (employeesEntered <= 200) {
+      participantMultiplier = 7;
+    } else if (employeesEntered <= 300) {
+      participantMultiplier = 6;
+    } else if (employeesEntered <= 400) {
+      participantMultiplier = 5;
+    } else if (employeesEntered <= 500) {
+      participantMultiplier = 4;
+    }
+  }
 
+  switch (planSelected.value) {
+    case "GO-Premier":
+      totalAnnualCost.textContent = `$${numberWithCommas(
+        (
+          amountOfW2Employees_value.value * 0.6 * participantMultiplier +
+          348
+        ).toFixed(2)
+      )}`;
+      break;
+    case "GO-Plus":
+      totalAnnualCost.textContent = `$${numberWithCommas(
+        (
+          amountOfW2Employees_value.value * 0.6 * participantMultiplier +
+          348
+        ).toFixed(2)
+      )}`;
+      break;
+    case "GO-Starter":
+      totalAnnualCost.textContent = `$${numberWithCommas(
+        (amountOfW2Employees_value.value * 0.6 * 9).toFixed(
+          2
+        )
+      )}`;
+      break;
+
+    default:
+      break;
+  }
+});
 
 document.querySelector("#calcForm").addEventListener("submit", (e) => {
   e.preventDefault();
